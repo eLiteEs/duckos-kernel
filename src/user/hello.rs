391e276e689245464d753f189d83d53bb381ca9d
Blas Fernández <elite.atencionalcliente@gmail.com>
2026-02-22 16:01:42 +0000

    Updated hello.rs

--- src/user/hello.rs
+++ src/user/hello.rs
@@ -6,25 +6,50 @@ const SYS_EXIT: u64 = 60;
 
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
-    let msg = b"Hola desde ELF64!\n";
-    
-    // Write syscall
+    // Mensaje 1
+    let msg1 = b"INICIO: Programa ejecutandose!\n";
     core::arch::asm!(
         "syscall",
         in("rax") SYS_WRITE,
         in("rdi") 1,
-        in("rsi") msg.as_ptr(),
-        in("rdx") msg.len(),
+        in("rsi") msg1.as_ptr(),
+        in("rdx") msg1.len(),
         lateout("rcx") _,
         lateout("r11") _,
         options(nostack)
     );
     
-    // Exit syscall
+    // Mensaje 2 - EL QUE ESPERAS
+    let msg2 = b"Hola desde DuckOS!\n";
+    core::arch::asm!(
+        "syscall",
+        in("rax") SYS_WRITE,
+        in("rdi") 1,
+        in("rsi") msg2.as_ptr(),
+        in("rdx") msg2.len(),
+        lateout("rcx") _,
+        lateout("r11") _,
+        options(nostack)
+    );
+    
+    // Mensaje 3
+    let msg3 = b"FIN: Saliendo...\n";
+    core::arch::asm!(
+        "syscall",
+        in("rax") SYS_WRITE,
+        in("rdi") 1,
+        in("rsi") msg3.as_ptr(),
+        in("rdx") msg3.len(),
+        lateout("rcx") _,
+        lateout("r11") _,
+        options(nostack)
+    );
+    
+    // Salir
     core::arch::asm!(
         "syscall",
         in("rax") SYS_EXIT,
-        in("rdi") 0,
+        in("rdi") 42,
         options(noreturn)
     );
 }
@@ -32,6 +57,18 @@ pub unsafe extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe {
+        let msg = b"PANIC en programa!\n";
+        core::arch::asm!(
+            "syscall",
+            in("rax") SYS_WRITE,
+            in("rdi") 2,
+            in("rsi") msg.as_ptr(),
+            in("rdx") msg.len(),
+            lateout("rcx") _,
+            lateout("r11") _,
+            options(nostack)
+        );
+        
         core::arch::asm!(
             "syscall",
             in("rax") SYS_EXIT,
