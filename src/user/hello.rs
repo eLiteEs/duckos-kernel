c331df8f41e36bee6ad8f0d38e729c03899ba616
Blas Fernández <elite.atencionalcliente@gmail.com>
2026-02-22 16:03:44 +0000

    Updated hello.rs again

--- src/user/hello.rs
+++ src/user/hello.rs
@@ -1,55 +1,32 @@
 #![no_std]
 #![no_main]
 
+// Usar números de syscall de Linux/x86_64
 const SYS_WRITE: u64 = 1;
 const SYS_EXIT: u64 = 60;
 
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
-    // Mensaje 1
-    let msg1 = b"INICIO: Programa ejecutandose!\n";
+    // Escribir directamente en el framebuffer? No, mejor usar syscall
+    let msg = b"HOLA\n";
+    
+    // Hacer syscall write(1, msg, 5)
     core::arch::asm!(
         "syscall",
         in("rax") SYS_WRITE,
         in("rdi") 1,
-        in("rsi") msg1.as_ptr(),
-        in("rdx") msg1.len(),
+        in("rsi") msg.as_ptr(),
+        in("rdx") msg.len(),
         lateout("rcx") _,
         lateout("r11") _,
         options(nostack)
     );
     
-    // Mensaje 2 - EL QUE ESPERAS
-    let msg2 = b"Hola desde DuckOS!\n";
-    core::arch::asm!(
-        "syscall",
-        in("rax") SYS_WRITE,
-        in("rdi") 1,
-        in("rsi") msg2.as_ptr(),
-        in("rdx") msg2.len(),
-        lateout("rcx") _,
-        lateout("r11") _,
-        options(nostack)
-    );
-    
-    // Mensaje 3
-    let msg3 = b"FIN: Saliendo...\n";
-    core::arch::asm!(
-        "syscall",
-        in("rax") SYS_WRITE,
-        in("rdi") 1,
-        in("rsi") msg3.as_ptr(),
-        in("rdx") msg3.len(),
-        lateout("rcx") _,
-        lateout("r11") _,
-        options(nostack)
-    );
-    
-    // Salir
+    // Syscall exit(0)
     core::arch::asm!(
         "syscall",
         in("rax") SYS_EXIT,
-        in("rdi") 42,
+        in("rdi") 0,
         options(noreturn)
     );
 }
@@ -57,18 +34,6 @@ pub unsafe extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe {
-        let msg = b"PANIC en programa!\n";
-        core::arch::asm!(
-            "syscall",
-            in("rax") SYS_WRITE,
-            in("rdi") 2,
-            in("rsi") msg.as_ptr(),
-            in("rdx") msg.len(),
-            lateout("rcx") _,
-            lateout("r11") _,
-            options(nostack)
-        );
-        
         core::arch::asm!(
             "syscall",
             in("rax") SYS_EXIT,
